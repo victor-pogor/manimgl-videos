d536926454da757843a4b1a57d808198182b03fd
Victor Pogor <victor.pogor@outlook.com>
2024-11-14 10:03:52 +0000

    post(two-body-problem): configured vectors

--- 2024/n_body_problem.py
+++ 2024/n_body_problem.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from manimlib import *
+from manimlib import *  # type: ignore
 import numpy as np
 
 
@@ -12,48 +12,73 @@ class TwoBodyProblem(InteractiveScene):
     vector1_fill_color: ManimColor = "#5d4e76"
     vector2_fill_color: ManimColor = "#4e5b2a"
 
-    # Constants
-    dot_radius: float = 0.06
-    circle_radius: float = 0.25
-
     # Phiysical properties
-    origin = np.array([0.0, 0.0, 0.0])
+    origin_position = np.array([0.0, 0.0, 0.0])
     body1_relative_position: Vect3 = np.array([-3.0, 0.5, 0.0])
-    body2_relative_position: Vect3 = np.array([0.5, 1.5, 0.0])
-    body1_position: Vect3 = origin + body1_relative_position
-    body2_position: Vect3 = origin + body2_relative_position
+    body2_relative_position: Vect3 = np.array([0.2, 1.5, 0.0])
+    body1_position: Vect3 = origin_position + body1_relative_position
+    body2_position: Vect3 = origin_position + body2_relative_position
     body1_velocity: Vect3 = np.array([3.0, -1.5, 0.0])
     body2_velocity: Vect3 = np.array([-0.5, -3.0, 0.0])
+    body1_mass: float = 1.0
+    body2_mass: float = 1.0
+
+    # Formulas
+    sum_of_masses: float = body1_mass + body2_mass  # M variable
+    distance_between_bodies: float = float(np.linalg.norm(body1_position - body2_position))  # r variable
+
+    # Bodies
+    body1_circle: Circle
+    body2_circle: Circle
+    body1_dot: Circle
+    body2_dot: Circle
+    position_vector1: Arrow
+    position_vector2: Arrow
+    vector_beetween_bodies: Arrow
+    origin_dot: Circle
+    origin_text: Text
+    baricenter_dot: Circle
+    baricenter_vector: Arrow
+    baricenter_text: Text
 
     def construct(self):
         self.camera.background_rgba = list(color_to_rgba(RED, 1))
 
-        # Plane
-        # axes = Axes(x_range=(-100, 100, 10), y_range=(-100, 100, 10))
-        # axes.add_coordinate_labels(font_size=48)
-
-        # plane = NumberPlane(
-        #     x_range=(-100, 100, 10), y_range=(-100, 100, 10), faded_line_ratio=0
-        # )
-        # self.add(plane, axes)
-
         # Bodies
-        body1_circle = self.__create_body_circle().move_to(self.body1_position)
-        body2_circle = self.__create_body_circle().move_to(self.body2_position)
-        self.add(body2_circle, body1_circle)
+        self.body1_circle = Circle(
+            radius=0.25,
+            fill_color=self.circle_fill_color,
+            fill_opacity=1,
+            stroke_color=self.circle_stroke_color,
+            stroke_opacity=0,
+        ).move_to(self.body1_position)
 
-        body1_dot = self.__create_body_dot().move_to(self.body1_position)
-        body2_dot = self.__create_body_dot().move_to(self.body2_position)
-        self.add(body1_dot, body2_dot)
+        self.body2_circle = Circle(
+            radius=0.25,
+            fill_color=self.circle_fill_color,
+            fill_opacity=1,
+            stroke_color=self.circle_stroke_color,
+            stroke_opacity=0,
+        ).move_to(self.body2_position)
 
-        # Vectors
-        # r_vector = Arrow(
-        #     start=self.body1_position, end=self.body2_position, buff=0
-        # )  # body1 <----- body2
-        # self.add(r_vector)
+        self.body1_dot = Circle(
+            radius=0.08,
+            fill_color=self.dot_fill_color,
+            fill_opacity=1,
+            stroke_color=self.dot_stroke_color,
+            stroke_opacity=0,
+        ).move_to(self.body1_position)
 
-        position_vector1 = Arrow(
-            start=self.origin,
+        self.body2_dot = Circle(
+            radius=0.08,
+            fill_color=self.dot_fill_color,
+            fill_opacity=1,
+            stroke_color=self.dot_stroke_color,
+            stroke_opacity=0,
+        ).move_to(self.body2_position)
+
+        self.position_vector1 = Arrow(
+            start=self.origin_position,
             end=self.body1_position,
             fill_color=self.vector2_fill_color,
             buff=0,
@@ -61,8 +86,8 @@ class TwoBodyProblem(InteractiveScene):
             max_width_to_length_ratio=1,
         )
 
-        position_vector2 = Arrow(
-            start=self.origin,
+        self.position_vector2 = Arrow(
+            start=self.origin_position,
             end=self.body2_position,
             fill_color=self.vector2_fill_color,
             buff=0,
@@ -70,7 +95,7 @@ class TwoBodyProblem(InteractiveScene):
             max_width_to_length_ratio=1,
         )
 
-        vector_beetween_bodies = Arrow(
+        self.vector_beetween_bodies = Arrow(
             start=self.body2_position,
             end=self.body1_position,
             fill_color=self.vector1_fill_color,
@@ -79,33 +104,54 @@ class TwoBodyProblem(InteractiveScene):
             max_width_to_length_ratio=1,
         )
 
-        self.add(position_vector1, position_vector2, vector_beetween_bodies)
-
-    # Create bodies
-    def __create_body_dot(
-        self,
-        radius=dot_radius,
-        fill_color: ManimColor = dot_fill_color,
-        stroke_color: ManimColor = dot_stroke_color,
-    ) -> Circle:
-        return Circle(
-            radius=radius,
-            fill_color=fill_color,
+        # Origin
+        self.origin_dot = Circle(
+            radius=0.1,
+            fill_color=self.vector1_fill_color,
             fill_opacity=1,
-            stroke_color=stroke_color,
+            stroke_color=self.dot_stroke_color,
             stroke_opacity=0,
+        ).move_to(self.origin_position)
+        self.origin_text = Text("O", font_size=36).next_to(self.origin_dot, np.array([0.2, -0.2, 0.0]))
+
+        # Baricenter
+        self.baricenter_dot = Circle(
+            radius=0.1,
+            fill_color=WHITE,
+            fill_opacity=0.5,
+            stroke_color=self.vector1_fill_color,
+            stroke_opacity=1,
+            stroke_width=2,
+        ).move_to(self.calculate_barycenter())
+
+        self.baricenter_vector = Arrow(
+            start=self.origin_position,
+            end=self.calculate_barycenter(),
+            fill_color=self.vector1_fill_color,
+            buff=0,
+            max_tip_length_to_length_ratio=1,
+            max_width_to_length_ratio=1,
         )
 
-    def __create_body_circle(
-        self,
-        radius=circle_radius,
-        fill_color: ManimColor = circle_fill_color,
-        stroke_color: ManimColor = circle_stroke_color,
-    ) -> Circle:
-        return Circle(
-            radius=radius,
-            fill_color=fill_color,
-            fill_opacity=1,
-            stroke_color=stroke_color,
-            stroke_opacity=0,
+        self.baricenter_text = Text("R", font_size=36).next_to(self.baricenter_vector.get_end(), np.array([0.0, -0.8, 0.0]))
+
+        # Add to scene
+        self.add(
+            self.body1_circle,
+            self.body2_circle,
+            self.body1_dot,
+            self.body2_dot,
+            self.position_vector1,
+            self.position_vector2,
+            self.vector_beetween_bodies,
+            self.origin_dot,
+            self.origin_text,
+            self.baricenter_dot,
+            self.baricenter_vector,
+            self.baricenter_text,
         )
+
+    def calculate_barycenter(self) -> Vect3:
+        """Calculate the barycenter (center of mass) of the two-body system."""
+        total_mass = self.body1_mass + self.body2_mass
+        return (self.body1_mass * self.body1_position + self.body2_mass * self.body2_position) / total_mass
